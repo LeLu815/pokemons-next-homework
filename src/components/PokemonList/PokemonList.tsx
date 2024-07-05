f8523287bed6cb1a990136bb1dab0700c485ef7a
LinQuixote <leein9404@naver.com>
2024-07-05 20:37:01 +0900

    feat : mui 로딩바 적용

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -6,6 +6,7 @@ import React, { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 
 import { usePokemonInfiniteQuery } from "@/react-query/usePokemonQuery";
+import CircularIndeterminate from "../Progress/Progress";
 
 function PokemonList() {
   const { ref, inView, entry } = useInView();
@@ -24,43 +25,45 @@ function PokemonList() {
   }, [fetchNextPage, inView]);
 
   if (isPending) {
-    return <div className="text-white">로딩중</div>;
+    return (
+      <div className="grid gap-3 min-[320px]:grid-cols-2 min-[500px]:grid-cols-3 sm:grid-cols-4 min-[900px]:grid-cols-5 lg:grid-cols-6"></div>
+    );
   }
   return (
-    <div className="grid gap-3 min-[320px]:grid-cols-2 min-[500px]:grid-cols-3 sm:grid-cols-4 min-[900px]:grid-cols-5 lg:grid-cols-6">
-      {pokemons &&
-        pokemons.pages.map((page, index) => (
-          <React.Fragment key={index}>
-            {page.map((pokemon) => (
-              <div
-                className="flex max-w-[270px] justify-center items-center border border-slate-500 aspect-square rounded-md cursor-pointer text-white"
-                key={pokemon.id}
-              >
-                <Link
-                  className="flex justify-center items-center flex-col"
-                  href={`/pokemon/${pokemon.id}`}
+    <>
+      <div className="grid gap-3 min-[320px]:grid-cols-2 min-[500px]:grid-cols-3 sm:grid-cols-4 min-[900px]:grid-cols-5 lg:grid-cols-6">
+        {pokemons &&
+          pokemons.pages.map((page, index) => (
+            <React.Fragment key={index}>
+              {page.map((pokemon) => (
+                <div
+                  className="flex max-w-[270px] justify-center items-center border border-slate-500 aspect-square rounded-md cursor-pointer text-white"
+                  key={pokemon.id}
                 >
-                  <Image
-                    alt={pokemon.korean_name}
-                    src={pokemon.sprites.front_default}
-                    width={96}
-                    height={96}
-                  />
-                  <p className="w-full text-left">{pokemon.korean_name}</p>
-                  <p className="font-extralight w-full text-left text-slate-400">
-                    도감번호 : {pokemon.id}
-                  </p>
-                </Link>
-              </div>
-            ))}
-          </React.Fragment>
-        ))}
-      <div className="h-20 flex items-center justify-center" ref={ref}>
-        {isFetchingNextPage && (
-          <div className="text-white">새로운 것 불러오는 중...</div>
-        )}
+                  <Link
+                    className="flex justify-center items-center flex-col"
+                    href={`/pokemon/${pokemon.id}`}
+                  >
+                    <Image
+                      alt={pokemon.korean_name}
+                      src={pokemon.sprites.front_default}
+                      width={96}
+                      height={96}
+                    />
+                    <p className="w-full text-left">{pokemon.korean_name}</p>
+                    <p className="font-extralight w-full text-left text-slate-400">
+                      도감번호 : {pokemon.id}
+                    </p>
+                  </Link>
+                </div>
+              ))}
+            </React.Fragment>
+          ))}
       </div>
-    </div>
+      <div className="h-[200px] flex items-center justify-center" ref={ref}>
+        {isFetchingNextPage && <CircularIndeterminate />}
+      </div>
+    </>
   );
 }
 
